a773bfc4b7775dd6438ddcbad708b6c0aa6563a4
Hauke Stieler <mail@hauke-stieler.de>
2018-06-13 22:52:20 +0200

    Refactor the default template
    The concept is: New lines above sections e.g. before the args-section or the commands-section. This will prevent a trailing blank line at the bottom of the usage message.

--- templates.go
+++ templates.go
@@ -38,24 +38,24 @@ var DefaultUsageTemplate = `{{define "FormatCommand"}}\
 {{end}}\
 
 {{if .Context.SelectedCommand}}\
-usage: {{.App.Name}} {{.Context.SelectedCommand}}{{template "FormatUsage" .Context.SelectedCommand}}
+usage: {{.App.Name}} {{.Context.SelectedCommand}}{{template "FormatUsage" .Context.SelectedCommand}}\
 {{else}}\
-usage: {{.App.Name}}{{template "FormatUsage" .App}}
+usage: {{.App.Name}}{{template "FormatUsage" .App}}\
 {{end}}\
-{{if .Context.Flags}}\
+{{if .Context.Flags}}
 Flags:
 {{.Context.Flags|FlagsToTwoColumns|FormatTwoColumns}}\
 {{end}}\
-{{if .Context.Args}}\
+{{if .Context.Args}}
 Args:
-{{.Context.Args|ArgsToTwoColumns|FormatTwoColumns}}
+{{.Context.Args|ArgsToTwoColumns|FormatTwoColumns}}\
 {{end}}\
 {{if .Context.SelectedCommand}}\
-{{if len .Context.SelectedCommand.Commands}}\
+{{if len .Context.SelectedCommand.Commands}}
 Subcommands:
-{{template "FormatCommands" .Context.SelectedCommand}}
+{{template "FormatCommands" .Context.SelectedCommand}}\
 {{end}}\
-{{else if .App.Commands}}\
+{{else if .App.Commands}}
 Commands:
 {{template "FormatCommands" .App}}\
 {{end}}\
